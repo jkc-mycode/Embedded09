9f1e3de00a915b02bccfaf51123d22feaf261a1d
jkc-mycode <90839233+jkc-mycode@users.noreply.github.com>
2022-12-14 14:21:10 +0900

    Update Security_enable.c

--- Security_enable.c
+++ Security_enable.c
@@ -21,19 +21,21 @@
 #define BT_RXD 1
 #define BT_TXD 0
 
-int SIGNAL = 0;     // bluetooth가 전송하는 설정 요구 값
-bool MODE = 0;      //모드 전역변수, bluetooth에서 직접 접근
+//int SIGNAL = 0;     // bluetooth가 전송하는 설정 요구 값
+bool MODE = 1;      //모드 전역변수, bluetooth에서 직접 접근
 bool WARNING = 0;   //보안 모드에서 현재 경보가 켜져있는지 아닌지
 int TIME_MODE = 2;  // 0 = day, 1 = hour, 2 = min
 float full_range = 0;
 float v_range = 0;
 float margin = 10.0f;
 int count = 0;
+int fd_serial;     // UART2 파일 서술자
+
 
 //ultra sonic sensor
 float Get_Range();
 void Set_Range();
-void Ultrasonic_Sensor(struct tm* comp_time);
+void *Ultrasonic_Sensor(void* t);
 
 //
 void Alert_On(struct tm* m_time);
@@ -42,27 +44,26 @@ void Counter(struct tm* m_time,struct tm* comp);
 
 static const char *UART2_DEV = "/dev/ttyAMA1"; // UART2 연결을 위한 장치 파일
 
-void Bluetooth();
+void *Bluetooth();
 unsigned char serialRead(const int fd);
 void serialWrite(const int fd, const unsigned char c);
 void serialWriteBytes(const int fd, const char *s);
 
 //외부 입력 처리
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t cond;
 
 int main(){
     //시간 기록을 위한 변수 (비갱신)
     time_t sys_time = time(NULL);
     struct tm checktime = *localtime(&sys_time);
+    pthread_t tid1, tid2;
     
     if(wiringPiSetupGpio() < 0){
         printf("wiringPiSetupGpio failed \n");
         return -1;
     }
-    // if(wiringPiI2CSetupInterface() < 0){
-    //     printf("wiringPiI2CSetUpInterface failed \n");
-    //     return -1;
-    // }
+    
     printf("wiringPi Setup Complete \n");
 
     pinMode(BUZZER, OUTPUT);
@@ -73,57 +74,81 @@ int main(){
     //거리 측정 (초기 셋팅)
     Set_Range();
 
-    printf("Device Function Started... \n");
+    printf("\nDevice Function Started... \n");
 
-    while (1)                       
-    {
-        Ultrasonic_Sensor(&checktime);
-    }
+    pthread_cond_init(&cond, NULL);
+
+    pthread_create(&tid1, NULL, Bluetooth, NULL);
+    pthread_create(&tid2, NULL, Ultrasonic_Sensor, &checktime);
+
+    pthread_join(tid1, NULL);
+    pthread_join(tid2, NULL);
+
+    pthread_mutex_destroy(&mutex);
     
     return 0;  
-    
 }
 
-void Ultrasonic_Sensor(struct tm* comp_time){
+void *Ultrasonic_Sensor(void* t){
+    while(1){
+    struct tm* comp_time = (struct tm*) t;
     float temp = 0;
     bool is_in = false;
-    bool t_mode = MODE;
+    bool t_mode;
     
+    printf("asdasd\n");
+
+    pthread_mutex_lock(&mutex);
+    
+    t_mode = MODE;
     if(t_mode == 0){        //mode useual
          while (1){
-            time_t sys_time = time(NULL);
-            struct tm maintime = *localtime(&sys_time);
-            printf("v_range : %f\n", v_range);
-            temp = Get_Range();
+            // time_t sys_time = time(NULL);
+            // struct tm maintime = *localtime(&sys_time);
+            // printf("v_range : %f\n", v_range);
+            // temp = Get_Range();
 
-            if(temp < v_range){
-                is_in = true;
-                //Counter(&maintime, comp_time, &count);
-            }else if((temp >= v_range) && (is_in == true)){
-                printf("4\n");
-                Counter(&maintime, comp_time);
-                is_in = false;
+            // if(temp < v_range){
+            //     is_in = true;
+            //     //Counter(&maintime, comp_time, &count);
+            // }else if((temp >= v_range) && (is_in == true)){
+            //     Counter(&maintime, comp_time);
+            //     is_in = false;
+            // }
+            // if(MODE != t_mode){
+            //     printf("MODE CHANGE : STATISTICS -> SECURITY \n");
+            //     break;
+            // }
+            // printf("Mode : %d\n", MODE);
+            if (serialDataAvail(fd_serial)){
+                pthread_cond_signal(&cond);
             }
-            if(MODE != t_mode){
-                printf("MODE CHANGE : STATISTICS -> SECURITY \n");
-                break;
-            }
+            printf("통계모드\n");
+            delay(100);
         }
     }else if(t_mode == 1){  //mode security
         while (1)
         {
-            time_t sys_time = time(NULL);
-            struct tm maintime = *localtime(&sys_time);
+            // time_t sys_time = time(NULL);
+            // struct tm maintime = *localtime(&sys_time);
 
-            temp = Get_Range();
-            if(temp < v_range){
-                Alert_On(&maintime);
+            // temp = Get_Range();
+            // if(temp < v_range){
+            //     Alert_On(&maintime);
+            // }
+            // if(MODE != t_mode){
+            //     printf("MODE CHANGE : SECURITY -> STATISTICS \n");
+            //     break;
+            // }
+            if (serialDataAvail(fd_serial)){
+                pthread_cond_signal(&cond);
             }
-            if(MODE != t_mode){
-                printf("MODE CHANGE : SECURITY -> STATISTICS \n");
-                break;
-            }
+
+            printf("보안모드\n");
+            delay(100);
         }
+    }
+    pthread_mutex_unlock(&mutex);
     }
 }
 
@@ -192,6 +217,7 @@ void Counter(struct tm* m_time,struct tm* comp){
             memcpy(&(*comp), &(*m_time), sizeof(struct tm));
         }
         count++;
+        printf("count : %d\n",count);
         break;
     default:
         printf("invalid time setting \n");
@@ -243,9 +269,11 @@ void serialWrite(const int fd, const unsigned char c)
     write(fd, &c, 1); // write 함수를 통해 1바이트 씀
 }
 
-void Bluetooth()
+void *Bluetooth()
 {
-    int fd_serial;     // UART2 파일 서술자
+    
+    // while(1) {
+    
     unsigned char dat; //데이터 임시 저장 변수
     FILE* fp; //파일 입출력 변수
     char temp; //기록 내용 담을 임시변수
@@ -254,53 +282,78 @@ void Bluetooth()
     if ((fd_serial = serialOpen(UART2_DEV, BAUD_RATE)) < 0)
     { // UART2 포트 오픈
         printf("Unable to open serial device.\n");
-        return;
     }
 
     while (1)
     {
+        
+        pthread_mutex_lock(&mutex);
+        
+        printf("asdasdasd\n");
         if (serialDataAvail(fd_serial))
         {                                //읽을 데이터가 존재한다면,
             dat = serialRead(fd_serial); //버퍼에서 1바이트 값을 읽음
             
-            switch(dat){
-                case '0': //일반모드
-                    MODE = 0;
-                    //초음파 센서 일반모드 함수 실행
-                    break;
-                case '1': //보안모드
-                    MODE = 1;
-                    //초음파 센서 보안모드 함수 실행
-                    break;
-                case '2': //초기설정 모드
-                    Set_Range();
-                    //초음파 센서 초기설정 함수 실행
-                    break;
-                case '3': //기록 파일 확인
-                    if((fp = fopen("./pass_history.txt", "r")) == NULL){ //파일 열리는지 확인
-                        return; 
-                    }
-                    while((fgets(file_buffer, sizeof(file_buffer), fp)) != NULL){ //파일이 끝날때 까지 반복
-                        serialWriteBytes(fd_serial, file_buffer); //휴대폰으로 파일의 할줄씩 출력
-                        printf("%s", file_buffer);
-                        memset(file_buffer, 0, sizeof(file_buffer)); //메모리 초기화
-                    }
-                    fclose(fp);
-                    break;
-                case '4': //기록 파일 초기화 (파일 내용 지움)
-                    if((fp = fopen("./pass_history.txt", "w")) == NULL){ //파일 열리는지 확인
-                        return; 
-                    }
-                    fputs(" ", fp);
-                    fclose(fp);
-                    break;
-                case '5': //경보 끄기
-                    //경보 on/off 할 함수 추가
-                    WARNING = FALSE;
-                    Alert_off();
-                    break;
+            if(dat == '0'){
+                MODE = 0;
+                //초음파 센서 일반모드 함수 실행
+                printf("000000000000000\n");
+                pthread_cond_wait(&cond, &mutex);
             }
+            else if(dat == '1'){
+                MODE = 1;
+                printf("1111111111111111\n");
+                //초음파 센서 보안모드 함수 실행
+                pthread_cond_wait(&cond, &mutex);
+            }
+
+            // switch(dat){
+            //     case '0': //일반모드
+            //         MODE = 0;
+            //         //초음파 센서 일반모드 함수 실행
+            //         printf("000000000000000\n");
+                    
+            //     case '1': //보안모드
+            //         MODE = 1;
+            //         printf("1111111111111111\n");
+            //         //초음파 센서 보안모드 함수 실행
+                    
+            //     case '2': //초기설정 모드
+            //         // Set_Range();
+            //         //초음파 센서 초기설정 함수 실행
+            //         printf("2\n");
+            //         break;
+            //     case '3': //기록 파일 확인
+            //         // if((fp = fopen("./pass_history.txt", "r")) == NULL){ //파일 열리는지 확인
+            //         //     break;
+            //         // }
+            //         // while((fgets(file_buffer, sizeof(file_buffer), fp)) != NULL){ //파일이 끝날때 까지 반복
+            //         //     serialWriteBytes(fd_serial, file_buffer); //휴대폰으로 파일의 할줄씩 출력
+            //         //     printf("%s", file_buffer);
+            //         //     memset(file_buffer, 0, sizeof(file_buffer)); //메모리 초기화
+            //         // }
+            //         // fclose(fp);
+            //         printf("3\n");
+            //         break;
+            //     case '4': //기록 파일 초기화 (파일 내용 지움)
+            //         // if((fp = fopen("./pass_history.txt", "w")) == NULL){ //파일 열리는지 확인
+            //         //     break;
+            //         // }
+            //         // fputs(" ", fp);
+            //         // fclose(fp);
+            //         printf("4\n");
+            //         break;
+            //     case '5': //경보 끄기
+            //         //경보 on/off 할 함수 추가
+            //         // WARNING = FALSE;
+            //         // Alert_off();
+            //         printf("5\n");
+            //         break;
+            // }
         }
         delay(10);
-    }    
+        pthread_mutex_unlock(&mutex);
+    } 
+
+    //}   
 }
